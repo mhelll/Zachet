4de29b0c4bcec8d2cc92c521c16d30fc47954f89
mhelll <92543258+mhelll@users.noreply.github.com>
2021-11-02 20:56:24 +0300

    Зачет 02.11

---
+++ 1.15.py
@@ -0,0 +1,6 @@
+x = int(input("Введите число: "))
+
+if not x > 0 and x < 5:
+   print(True)
+else:
+   print(False)
---
+++ 4.5.py
@@ -0,0 +1,4 @@
+a = range(-10, 10)
+
+print(len([i for i in a if i < 0]))
+print(sum([i for i in a if i < 0]))
